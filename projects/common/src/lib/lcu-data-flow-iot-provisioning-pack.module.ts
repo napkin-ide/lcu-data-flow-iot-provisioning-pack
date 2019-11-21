d6e8e70f929644e2c7628a72c77dbad6419bfa89
mcgear <michael.gearhardt@fathym.com>
2019-11-21 11:48:50 -0700

    Upgraded

--- projects/common/src/lib/lcu-data-flow-iot-provisioning-pack.module.ts
+++ projects/common/src/lib/lcu-data-flow-iot-provisioning-pack.module.ts
@@ -1,12 +1,16 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-
+import { NgModule, ModuleWithProviders } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule]
 })
-export class LcuDataFlowIotProvisioningPackModule { }
+export class LcuDataFlowIotProvisioningPackModule {
+  public static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: LcuDataFlowIotProvisioningPackModule,
+      providers: [
+      ]
+    };
+  }
+}
--- projects/lcu/src/app/app.module.ts
+++ projects/lcu/src/app/app.module.ts
@@ -1,12 +1,23 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-
+import { NgModule, DoBootstrap, Injector } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { createCustomElement } from '@angular/elements';
+import { FathymSharedModule, LCUServiceSettings, JSONSchema } from '@lcu/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { LcuDataFlowIotProvisioningPackModule } from '@napkin-ide/lcu-data-flow-iot-provisioning-pack-common';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
+  imports: [BrowserModule, BrowserAnimationsModule, FathymSharedModule, LcuDataFlowIotProvisioningPackModule.forRoot()],
+  exports: [LcuDataFlowIotProvisioningPackModule],
+  providers: [
   ]
 })
-export class AppModule { }
+export class AppModule implements DoBootstrap {
+  //  Constructors
+  constructor(protected injector: Injector) {
+  }
+
+  //  Life Cycle
+  public ngDoBootstrap() {
+  }
+}
