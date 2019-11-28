96c503442c284fa4e7a4ba217bc38b6e40d2d51f
mcgear <michael.gearhardt@fathym.com>
2019-11-28 01:26:50 -0700

    Some additional base imports for common

--- projects/common/src/lib/lcu-data-flow-iot-provisioning-pack.module.ts
+++ projects/common/src/lib/lcu-data-flow-iot-provisioning-pack.module.ts
@@ -1,11 +1,15 @@
-import { NgModule, ModuleWithProviders } from "@angular/core";
-import { CommonModule } from "@angular/common";
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { LcuDataFlowIotProvisioningPackDataStreamElementComponent } from './elements/data-stream/data-stream.component';
 import { LcuDataFlowIotProvisioningPackDeviceStreamElementComponent } from './elements/device-stream/device-stream.component';
 import { LcuDataFlowIotProvisioningPackColdStorageElementComponent } from './elements/cold-storage/cold-storage.component';
 import { LcuDataFlowIotProvisioningPackWarmStorageElementComponent } from './elements/warm-storage/warm-storage.component';
 import { LcuDataFlowIotProvisioningPackDataMapElementComponent } from './elements/data-map/data-map.component';
 import { LcuDataFlowIotProvisioningPackHotStorageElementComponent } from './elements/hot-storage/hot-storage.component';
+import { FathymSharedModule, MaterialModule } from '@lcu/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
@@ -32,14 +36,20 @@ import { LcuDataFlowIotProvisioningPackHotStorageElementComponent } from './elem
     LcuDataFlowIotProvisioningPackDataMapElementComponent,
     LcuDataFlowIotProvisioningPackHotStorageElementComponent
   ],
-  imports: [CommonModule]
+  imports: [
+    FathymSharedModule,
+    MaterialModule,
+    FlexLayoutModule,
+    ReactiveFormsModule,
+    FormsModule,
+    ChartsModule
+  ]
 })
 export class LcuDataFlowIotProvisioningPackModule {
   public static forRoot(): ModuleWithProviders {
     return {
       ngModule: LcuDataFlowIotProvisioningPackModule,
-      providers: [
-      ]
+      providers: []
     };
   }
 }
