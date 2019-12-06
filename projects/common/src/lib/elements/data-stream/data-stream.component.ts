dc373e743a3076da65c3da3c1fa8ebd343f2a66b
mcgear <michael.gearhardt@gmail.com>
2019-12-06 07:06:33 -0700

    Adjusted

--- projects/common/src/lib/elements/data-stream/data-stream.component.ts
+++ projects/common/src/lib/elements/data-stream/data-stream.component.ts
@@ -1,21 +1,36 @@
-import { Component, OnInit, Injector } from "@angular/core";
-import { LCUElementContext, LcuElementComponent } from "@lcu/common";
+import { Component, OnInit, Injector } from '@angular/core';
+import {
+  LCUElementContext,
+  LcuElementComponent,
+  DataFlowModule
+} from '@lcu/common';
+import { Color, Label } from 'ng2-charts';
+import { ChartOptions } from 'chart.js';
 
 export class LcuDataFlowIotProvisioningPackDataStreamElementState {
-  public SomeProperty?: string;
+  public APIKeys?: { [name: string]: string };
+
+  public Chart: {
+    Results: any[];
+    Colors: Color[];
+    Labels: Label[];
+    Options: ChartOptions & { annotation: any };
+  };
 }
 
 export class LcuDataFlowIotProvisioningPackDataStreamContext extends LCUElementContext<
   LcuDataFlowIotProvisioningPackDataStreamElementState
-> {}
+> {
+  public Module: DataFlowModule;
+}
 
 export const SelectorLcuDataFlowIotProvisioningPackDataStreamElement =
-  "lcu-data-flow-iot-provisioning-pack-data-stream-element";
+  'lcu-data-flow-iot-provisioning-pack-data-stream-element';
 
 @Component({
   selector: SelectorLcuDataFlowIotProvisioningPackDataStreamElement,
-  templateUrl: "./data-stream.component.html",
-  styleUrls: ["./data-stream.component.scss"]
+  templateUrl: './data-stream.component.html',
+  styleUrls: ['./data-stream.component.scss']
 })
 export class LcuDataFlowIotProvisioningPackDataStreamElementComponent
   extends LcuElementComponent<LcuDataFlowIotProvisioningPackDataStreamContext>
@@ -32,18 +47,10 @@ export class LcuDataFlowIotProvisioningPackDataStreamElementComponent
   //  Life Cycle
   public ngOnInit() {
     super.ngOnInit();
-
-    // if (this.context) {
-    //   if (!this.context.State) {
-    //     this.context.State = {};
-    //   }
-    // }
   }
 
   //  API Methods
-  public Update() {
-    this.context.State.SomeProperty = new Date().toString();
-  }
+  public Update() {}
 
   //  Helpers
 }
