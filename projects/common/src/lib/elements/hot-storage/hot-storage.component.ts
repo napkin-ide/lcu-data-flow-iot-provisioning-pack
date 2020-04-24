e76fc79c34e5a090f1ec8d7895d2f0bc52ce639b
xchanin <me@shannonbruns.com>
2020-04-24 14:51:10 -0600

    add connection string to hot storage

--- projects/common/src/lib/elements/hot-storage/hot-storage.component.ts
+++ projects/common/src/lib/elements/hot-storage/hot-storage.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit, Injector } from '@angular/core';
 import { LCUElementContext, LcuElementComponent } from '@lcu/common';
+import { InfrastructureModel } from '../../models/infrastructure.model';
 
-export class LcuDataFlowIotProvisioningPackHotStorageElementState {}
+export class LcuDataFlowIotProvisioningPackHotStorageElementState {
+  public Infrastructure: InfrastructureModel;
+}
 
-export class LcuDataFlowIotProvisioningPackHotStorageContext extends LCUElementContext<LcuDataFlowIotProvisioningPackHotStorageElementState> {}
+export class LcuDataFlowIotProvisioningPackHotStorageContext extends 
+LCUElementContext<LcuDataFlowIotProvisioningPackHotStorageElementState> {}
 
 export const SelectorLcuDataFlowIotProvisioningPackHotStorageElement = 'lcu-data-flow-iot-provisioning-pack-hot-storage-element';
 
@@ -12,14 +16,21 @@ export const SelectorLcuDataFlowIotProvisioningPackHotStorageElement = 'lcu-data
   templateUrl: './hot-storage.component.html',
   styleUrls: ['./hot-storage.component.scss']
 })
-export class LcuDataFlowIotProvisioningPackHotStorageElementComponent extends LcuElementComponent<LcuDataFlowIotProvisioningPackHotStorageContext> implements OnInit {
+export class LcuDataFlowIotProvisioningPackHotStorageElementComponent extends 
+LcuElementComponent<LcuDataFlowIotProvisioningPackHotStorageContext> implements OnInit {
   //  Fields
 
   //  Properties
+  /**
+   * Connection string title
+   */
+  public Title: string;
 
   //  Constructors
   constructor(protected injector: Injector) {
     super(injector);
+
+    this.Title = 'Hot Storage Connection Strings';
   }
 
   //  Life Cycle
