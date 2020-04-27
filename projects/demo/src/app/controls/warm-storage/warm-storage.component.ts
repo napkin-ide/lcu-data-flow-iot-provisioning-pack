e312a648fc5c120c2f0cec58ef096b00019e3f19
xchanin <me@shannonbruns.com>
2020-04-27 15:20:20 -0600

    if it doesnt fit

--- projects/demo/src/app/controls/warm-storage/warm-storage.component.ts
+++ projects/demo/src/app/controls/warm-storage/warm-storage.component.ts
@@ -11,70 +11,12 @@ import { LcuDataFlowIotProvisioningPackWarmStorageElementState } from '@napkin-i
 export class WarmStorageComponent implements OnInit {
 
   public WarmStorageState: LcuDataFlowIotProvisioningPackWarmStorageElementState;
-  public LCUChart: LCUChart;
+  // public LCUChart: LCUChart;
   constructor() { }
 
   ngOnInit() {
 
     this.WarmStorageState = {
-      Chart: {
-        Results: [
-          { data: [10, 8, 12, 14, 9, 10, 1], label: 'Success' },
-          { data: [0, 0, 1, 0, 0, 2, 0], label: 'Error' }
-        ],
-        Colors: [
-          {
-            // dark grey
-            backgroundColor: 'rgba(77,83,96,0.2)',
-            borderColor: 'rgba(77,83,96,1)',
-            pointBackgroundColor: 'rgba(77,83,96,1)',
-            pointBorderColor: '#fff',
-            pointHoverBackgroundColor: '#fff',
-            pointHoverBorderColor: 'rgba(77,83,96,1)'
-          },
-          {
-            // red
-            backgroundColor: 'rgba(255,0,0,0.3)',
-            borderColor: 'red',
-            pointBackgroundColor: 'rgba(148,159,177,1)',
-            pointBorderColor: '#fff',
-            pointHoverBackgroundColor: '#fff',
-            pointHoverBorderColor: 'rgba(148,159,177,0.8)'
-          }
-        ],
-        Labels: ['1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'],
-        Options: {
-          maintainAspectRatio: false,
-          responsive: true,
-          scales: {
-            // We use this empty structure as a placeholder for dynamic theming.
-            xAxes: [{}],
-            yAxes: [
-              {
-                id: 'y-axis-0',
-                position: 'left'
-              }
-            ]
-          },
-          annotation: {
-            annotations: [
-              {
-                type: 'line',
-                mode: 'vertical',
-                scaleID: 'x-axis-0',
-                value: 'March',
-                borderColor: 'orange',
-                borderWidth: 2,
-                label: {
-                  enabled: true,
-                  fontColor: 'orange',
-                  content: 'LineAnno'
-                }
-              }
-            ]
-          }
-        }
-      },
       Infrastructure: {
         Connections: {
           // tslint:disable-next-line:max-line-length
