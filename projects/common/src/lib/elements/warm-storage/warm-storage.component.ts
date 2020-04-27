4efbb2c824076abd8fce0df71a052656d7c26a8b
xchanin <me@shannonbruns.com>
2020-04-27 14:57:46 -0600

    revert

--- projects/common/src/lib/elements/warm-storage/warm-storage.component.ts
+++ projects/common/src/lib/elements/warm-storage/warm-storage.component.ts
@@ -1,70 +1,15 @@
-// import { InfrastructureModel } from './../../models/infrastructure.model';
-// import { Component, OnInit, Injector } from '@angular/core';
-// import { LCUElementContext, LcuElementComponent } from '@lcu/common';
-
-// export class LcuDataFlowIotProvisioningPackWarmStorageElementState {
-//   public Infrastructure: InfrastructureModel;
-// }
-
-// export class LcuDataFlowIotProvisioningPackWarmStorageContext extends
-// LCUElementContext<LcuDataFlowIotProvisioningPackWarmStorageElementState> {}
-
-// export const SelectorLcuDataFlowIotProvisioningPackWarmStorageElement = 'lcu-data-flow-iot-provisioning-pack-warm-storage-element';
-
-// @Component({
-//   selector: SelectorLcuDataFlowIotProvisioningPackWarmStorageElement,
-//   templateUrl: './warm-storage.component.html',
-//   styleUrls: ['./warm-storage.component.scss']
-// })
-
-// export class LcuDataFlowIotProvisioningPackWarmStorageElementComponent extends
-// LcuElementComponent<LcuDataFlowIotProvisioningPackWarmStorageContext> implements OnInit {
-//   //  Fields
-
-//   //  Properties
-
-//   /**
-//    * Connection string title
-//    */
-//   public Title: string;
-
-//   //  Constructors
-//   constructor(protected injector: Injector) {
-//     super(injector);
-
-//     this.Title = 'Warm Storage Connection Strings';
-//   }
-
-//   //  Life Cycle
-//   public ngOnInit() {
-//     super.ngOnInit();
-//   }
-
-//   //  API Methods
-
-//   //  Helpers
-// }
-
-
-import { LCUChart } from './../../models/chart';
 import { InfrastructureModel } from './../../models/infrastructure.model';
-import { Component, OnInit, Injector, AfterViewInit } from '@angular/core';
+import { Component, OnInit, Injector } from '@angular/core';
 import { LCUElementContext, LcuElementComponent } from '@lcu/common';
-import { TabComponentModel } from '../../models/component-tab.model';
-import { ConnectionStringsComponent } from '../../controls/connection-strings/connection-strings.component';
-import { ChartsComponent } from '../../controls/charts/charts.component';
 
 export class LcuDataFlowIotProvisioningPackWarmStorageElementState {
-  public Chart: LCUChart;
-
   public Infrastructure: InfrastructureModel;
 }
 
 export class LcuDataFlowIotProvisioningPackWarmStorageContext extends
 LCUElementContext<LcuDataFlowIotProvisioningPackWarmStorageElementState> {}
 
-export const SelectorLcuDataFlowIotProvisioningPackWarmStorageElement =
-'lcu-data-flow-iot-provisioning-pack-warm-storage-element';
+export const SelectorLcuDataFlowIotProvisioningPackWarmStorageElement = 'lcu-data-flow-iot-provisioning-pack-warm-storage-element';
 
 @Component({
   selector: SelectorLcuDataFlowIotProvisioningPackWarmStorageElement,
@@ -73,12 +18,10 @@ export const SelectorLcuDataFlowIotProvisioningPackWarmStorageElement =
 })
 
 export class LcuDataFlowIotProvisioningPackWarmStorageElementComponent extends
-LcuElementComponent<LcuDataFlowIotProvisioningPackWarmStorageContext> implements OnInit, AfterViewInit {
+LcuElementComponent<LcuDataFlowIotProvisioningPackWarmStorageContext> implements OnInit {
   //  Fields
 
   //  Properties
-
-  public Components: Array<TabComponentModel>;
 
   /**
    * Connection string title
@@ -93,39 +36,96 @@ LcuElementComponent<LcuDataFlowIotProvisioningPackWarmStorageContext> implements
   }
 
   //  Life Cycle
-  public ngOnInit(): void {
+  public ngOnInit() {
     super.ngOnInit();
-  }
-
-  public ngAfterViewInit(): void {
-
-    if (!this.context) {
-      console.log('no warm storage data');
-      return;
-    }
-
-    this.setupComponents();
   }
 
   //  API Methods
 
   //  Helpers
-
-  public GetStrings(): void {
-    this.setupComponents();
-  }
-
-  /**
-   * Set components for this module
-   */
-  protected setupComponents(): void {
-    this.Components = [
-      new TabComponentModel({ Component: ChartsComponent,
-                              Data: this.context.State.Chart,
-                              Label: 'Overview' }),
-      new TabComponentModel({ Component: ConnectionStringsComponent,
-                              Data: this.context.State.Infrastructure.Connections,
-                              Label: 'Connection Strings' })
-    ];
-  }
 }
+
+
+// import { LCUChart } from './../../models/chart';
+// import { InfrastructureModel } from './../../models/infrastructure.model';
+// import { Component, OnInit, Injector, AfterViewInit } from '@angular/core';
+// import { LCUElementContext, LcuElementComponent } from '@lcu/common';
+// import { TabComponentModel } from '../../models/component-tab.model';
+// import { ConnectionStringsComponent } from '../../controls/connection-strings/connection-strings.component';
+// import { ChartsComponent } from '../../controls/charts/charts.component';
+
+// export class LcuDataFlowIotProvisioningPackWarmStorageElementState {
+//   public Chart: LCUChart;
+
+//   public Infrastructure: InfrastructureModel;
+// }
+
+// export class LcuDataFlowIotProvisioningPackWarmStorageContext extends
+// LCUElementContext<LcuDataFlowIotProvisioningPackWarmStorageElementState> {}
+
+// export const SelectorLcuDataFlowIotProvisioningPackWarmStorageElement =
+// 'lcu-data-flow-iot-provisioning-pack-warm-storage-element';
+
+// @Component({
+//   selector: SelectorLcuDataFlowIotProvisioningPackWarmStorageElement,
+//   templateUrl: './warm-storage.component.html',
+//   styleUrls: ['./warm-storage.component.scss']
+// })
+
+// export class LcuDataFlowIotProvisioningPackWarmStorageElementComponent extends
+// LcuElementComponent<LcuDataFlowIotProvisioningPackWarmStorageContext> implements OnInit, AfterViewInit {
+//   //  Fields
+
+//   //  Properties
+
+//   public Components: Array<TabComponentModel>;
+
+//   /**
+//    * Connection string title
+//    */
+//   public Title: string;
+
+//   //  Constructors
+//   constructor(protected injector: Injector) {
+//     super(injector);
+
+//     this.Title = 'Warm Storage Connection Strings';
+//   }
+
+//   //  Life Cycle
+//   public ngOnInit(): void {
+//     super.ngOnInit();
+//   }
+
+//   public ngAfterViewInit(): void {
+
+//     if (!this.context) {
+//       console.log('no warm storage data');
+//       return;
+//     }
+
+//     this.setupComponents();
+//   }
+
+//   //  API Methods
+
+//   //  Helpers
+
+//   public GetStrings(): void {
+//     this.setupComponents();
+//   }
+
+//   /**
+//    * Set components for this module
+//    */
+//   protected setupComponents(): void {
+//     this.Components = [
+//       new TabComponentModel({ Component: ChartsComponent,
+//                               Data: this.context.State.Chart,
+//                               Label: 'Overview' }),
+//       new TabComponentModel({ Component: ConnectionStringsComponent,
+//                               Data: this.context.State.Infrastructure.Connections,
+//                               Label: 'Connection Strings' })
+//     ];
+//   }
+// }
