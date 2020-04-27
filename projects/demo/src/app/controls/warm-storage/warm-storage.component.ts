aad4a6dba34950dd71d40d1d4ad8d25a2838d6bc
xchanin <me@shannonbruns.com>
2020-04-27 17:07:49 -0600

    fix warmstoragestate

--- projects/demo/src/app/controls/warm-storage/warm-storage.component.ts
+++ projects/demo/src/app/controls/warm-storage/warm-storage.component.ts
@@ -16,20 +16,18 @@ export class WarmStorageComponent implements OnInit {
 
   ngOnInit() {
 
-    this.WarmStorageState = {
-      Infrastructure: {
-        Connections: {
-          // tslint:disable-next-line:max-line-length
-          'Primary SQL Connection String': 'AccountEndpoint=https://lcu-int.documents.azure.com:443/;AccountKey=Qan4GIhRTovYZmdogrsZnYDR8xiI0RL6gvGa9ufnp3c4RGRit0xAmasqiEOEwqPQs3HoN60w8NyM5E6DbW96yA==;',
-          'Secondary SQL Connection String': 'AccountEndpoint=https://lcu-int.documents.azure.com:443/;AccountKey=wBIakKZVZ1f5r4csg5Beu3lYOeoHinMdYtWNVt3gdvQ6jXebdWRALdAC3MHZKv2uEw35hRzM57QoPXGoj8CAcg==;',
-          'Primary Read-Only SQL Connection String': 'AccountEndpoint=https://lcu-int.documents.azure.com:443/;AccountKey=6gcv6sCq9k28srWauxeDSBR9Zork7DaWZGcOWU9VdRMzK8WwCtm3gigDMtUzURgkEw0pH0bLoB26joiaYiM72A==;',
-          'Secondary Read-Only SQL Connection String': 'AccountEndpoint=https://lcu-int.documents.azure.com:443/;AccountKey=wWW6Eadn8Cgs3QG1TMKajyjV54XkK7TttDyV1HX2SKzBGzAMVef6rKf87uYQQt8KVZpyUug8UTTzsLabqZGbNw==;',
-          'Primary Gremlin Connection String': 'AccountEndpoint=https://lcu-int.documents.azure.com:443/;AccountKey=Qan4GIhRTovYZmdogrsZnYDR8xiI0RL6gvGa9ufnp3c4RGRit0xAmasqiEOEwqPQs3HoN60w8NyM5E6DbW96yA==;ApiKind=Gremlin;',
-          'Secondary Gremlin Connection String': 'AccountEndpoint=https://lcu-int.documents.azure.com:443/;AccountKey=wBIakKZVZ1f5r4csg5Beu3lYOeoHinMdYtWNVt3gdvQ6jXebdWRALdAC3MHZKv2uEw35hRzM57QoPXGoj8CAcg==;ApiKind=Gremlin;',
-          'Primary Read-Only Gremlin Connection String': 'AccountEndpoint=https://lcu-int.documents.azure.com:443/;AccountKey=6gcv6sCq9k28srWauxeDSBR9Zork7DaWZGcOWU9VdRMzK8WwCtm3gigDMtUzURgkEw0pH0bLoB26joiaYiM72A==;ApiKind=Gremlin;',
-          'Secondary Read-Only Gremlin Connection String': 'AccountEndpoint=https://lcu-int.documents.azure.com:443/;AccountKey=wWW6Eadn8Cgs3QG1TMKajyjV54XkK7TttDyV1HX2SKzBGzAMVef6rKf87uYQQt8KVZpyUug8UTTzsLabqZGbNw==;ApiKind=Gremlin;'
-        }
-      }
-    };
+    // this.WarmStorageState = {
+    //   Infrastructure: {
+    //     Connections: {          'Primary SQL Connection String': 'AccountEndpoint=https://lcu-int.documents.azure.com:443/;AccountKey=Qan4GIhRTovYZmdogrsZnYDR8xiI0RL6gvGa9ufnp3c4RGRit0xAmasqiEOEwqPQs3HoN60w8NyM5E6DbW96yA==;',
+    //       'Secondary SQL Connection String': 'AccountEndpoint=https://lcu-int.documents.azure.com:443/;AccountKey=wBIakKZVZ1f5r4csg5Beu3lYOeoHinMdYtWNVt3gdvQ6jXebdWRALdAC3MHZKv2uEw35hRzM57QoPXGoj8CAcg==;',
+    //       'Primary Read-Only SQL Connection String': 'AccountEndpoint=https://lcu-int.documents.azure.com:443/;AccountKey=6gcv6sCq9k28srWauxeDSBR9Zork7DaWZGcOWU9VdRMzK8WwCtm3gigDMtUzURgkEw0pH0bLoB26joiaYiM72A==;',
+    //       'Secondary Read-Only SQL Connection String': 'AccountEndpoint=https://lcu-int.documents.azure.com:443/;AccountKey=wWW6Eadn8Cgs3QG1TMKajyjV54XkK7TttDyV1HX2SKzBGzAMVef6rKf87uYQQt8KVZpyUug8UTTzsLabqZGbNw==;',
+    //       'Primary Gremlin Connection String': 'AccountEndpoint=https://lcu-int.documents.azure.com:443/;AccountKey=Qan4GIhRTovYZmdogrsZnYDR8xiI0RL6gvGa9ufnp3c4RGRit0xAmasqiEOEwqPQs3HoN60w8NyM5E6DbW96yA==;ApiKind=Gremlin;',
+    //       'Secondary Gremlin Connection String': 'AccountEndpoint=https://lcu-int.documents.azure.com:443/;AccountKey=wBIakKZVZ1f5r4csg5Beu3lYOeoHinMdYtWNVt3gdvQ6jXebdWRALdAC3MHZKv2uEw35hRzM57QoPXGoj8CAcg==;ApiKind=Gremlin;',
+    //       'Primary Read-Only Gremlin Connection String': 'AccountEndpoint=https://lcu-int.documents.azure.com:443/;AccountKey=6gcv6sCq9k28srWauxeDSBR9Zork7DaWZGcOWU9VdRMzK8WwCtm3gigDMtUzURgkEw0pH0bLoB26joiaYiM72A==;ApiKind=Gremlin;',
+    //       'Secondary Read-Only Gremlin Connection String': 'AccountEndpoint=https://lcu-int.documents.azure.com:443/;AccountKey=wWW6Eadn8Cgs3QG1TMKajyjV54XkK7TttDyV1HX2SKzBGzAMVef6rKf87uYQQt8KVZpyUug8UTTzsLabqZGbNw==;ApiKind=Gremlin;'
+    //     }
+    //   }
+    // };
   }
 }
